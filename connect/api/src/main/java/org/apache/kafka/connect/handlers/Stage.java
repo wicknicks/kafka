608c709bd0a279d81ce30a726b101c307d80ce81
Arjun Satish <arjun@confluent.io>
2018-04-27 13:35:54 -0700

    Add Copyright
    Signed-off-by: Arjun Satish <arjun@confluent.io>

--- connect/api/src/main/java/org/apache/kafka/connect/handlers/Stage.java
+++ connect/api/src/main/java/org/apache/kafka/connect/handlers/Stage.java
@@ -1,9 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.connect.handlers;
 
 /**
  * A logical stage in a Connect pipeline
  */
 public enum Stage {
+
+    /**
+     * When the task starts up
+     */
+    TASK_START,
 
     /**
      * when deserializing messages from Kafka into ConnectRecords
@@ -28,6 +49,16 @@ public enum Stage {
     /**
      * when serializing ConnectRecords to be produced into Kafka
      */
-    SERIALIZATION
+    SERIALIZATION,
+
+    /**
+     * when the worker is committing offsets for the task
+     */
+    COMMIT_OFFSETS,
+
+    /**
+     * When the task is shutting down
+     */
+    TASK_CLOSE,
 
 }
